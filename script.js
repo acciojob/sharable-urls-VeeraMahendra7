7779912f04a69ad1e450939295b7dcb30eae5458
VeeraMahendra7 <140475884+VeeraMahendra7@users.noreply.github.com>
2025-10-15 15:25:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // your code here
+let form = document.getElementById('myForm');
+let h3 = document.getElementById('url');
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	let input1 = document.getElementById('name').value.trim();
+	let input2 = document.getElementById('year').value.trim();
+	
+	const baseURL = 'https://localhost:8080/';
+	const query = `?name=${encodeURIComponent(name)}&year=${encodeURIComponent(year)}`;
+	h3.textContent = baseURL + query; 
+	
+});
